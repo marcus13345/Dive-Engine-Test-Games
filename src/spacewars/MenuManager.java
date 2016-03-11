03a9467bc99b848b5a6ba87ef1e4c9f3e67ec725
Marcus Gosselin <marcus13345@live.com>
2016-03-11 00:10:51 -0500

    started implementing buttons

--- src/spacewars/MenuManager.java
+++ src/spacewars/MenuManager.java
@@ -1,9 +1,15 @@
 package spacewars;
 
 import diveengine2d.DiveScript;
+import diveengine2d.SceneManager;
 
 /**
  * Created by Marcus on 3/10/2016.
  */
 public class MenuManager extends DiveScript {
+    public void playClicked() {
+
+        SceneManager.loadScene("game");
+
+    }
 }
